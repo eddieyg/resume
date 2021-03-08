5d263e2611137a4d2379c477e1def0910150b545
eddieyg <eddiezhao@foxmail.com>
2021-03-08 23:31:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Main />
+</template>
+
+<script setup>
+import Main from './views/main.vue'
+import { onMounted } from "vue";
+const importStyle = () => import("./assets/style.less")
+
+onMounted(async () => {
+  const style = (await importStyle()).default;
+  console.log(style);
+});
+</script>
+
+<style>
+#app {}
+</style>
---
+++ src/views/main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main-container">
+    <h1>my resume main page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.main-container{
+  >h1{
+    text-align: center;
+  }
+}
+</style>
